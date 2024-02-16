1bcd6eaa0dd8a85eba28d96a6f61189f780b1a46
deepakkumar27J <90575935+deepakkumar27J@users.noreply.github.com>
2024-02-16 13:57:48 +0000

    App files added first time after initial

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using reviewAppWebAPI.Data;
+using reviewAppWebAPI.Interfaces;
+using reviewAppWebAPI.Model;
+
+namespace reviewAppWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly DataContext context;
+
+        public PokemonController(IPokemonRepository pokemonRepository, DataContext context)
+        {
+            _pokemonRepository = pokemonRepository;
+            this.context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons() 
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            return Ok(pokemons);
+
+        }
+    }
+}
---
+++ Model/Country.cs
@@ -0,0 +1,9 @@
+﻿namespace reviewAppWebAPI.Model
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Owner> Owners { get; set; }
+    }
+}
