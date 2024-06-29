ffe2058b3a69348ea27a807b7c5d9ad3219f2464
deepakkumar27J <90575935+deepakkumar27J@users.noreply.github.com>
2024-06-30 00:47:55 +0100

    user management added api for register, and login

---
+++ Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using reviewAppWebAPI.Interfaces;
+using reviewAppWebAPI.Model;
+using System.Threading.Tasks;
+
+namespace reviewAppWebAPI.Repository
+{
+    public class UserRepository: IUserRepository
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserRepository(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<ApplicationUser> FindByEmailAsync(string email)
+        {
+            return await _userManager.FindByEmailAsync(email);
+        }
+
+        public async Task<bool> CreateAsync(ApplicationUser user, string password)
+        {
+            var result = await _userManager.CreateAsync(user, password);
+            return result.Succeeded;
+        }
+
+        public async Task<bool> CheckPasswordAsync(ApplicationUser user, string password)
+        {
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+    }
+}
